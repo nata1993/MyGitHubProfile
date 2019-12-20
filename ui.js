859ca7f561a7259bfd34b23028d5e1c8c80a78ee
Opilane TTHK <opilane@tthk.ee>
2019-12-20 11:50:13 +0200

    added: badges - followers, following, repos

--- ui.js
+++ ui.js
@@ -14,7 +14,13 @@ class UI
                     <h3 class="font-weight-bold text-info">${user.login}</h3>
                     <img class="img-fluid mb-2" src="${user.avatar_url}">
                     <a class="btn btn-primary btn-block" target="_blank" href="${user.html_url}"> Go to Profile </a>
-                </div>                            
+                </div>
+
+                <div class="col-md-9">
+                    <span class="badge badge-primary"> Public Repos: ${user.public_repos}</span>
+                    <span class="badge badge-success"> Followers: ${user.followers}</span>
+                    <span class="badge badge-warning"> Following: ${user.following}</span>
+                </div>                          
             </div>
         </div>
         `;
