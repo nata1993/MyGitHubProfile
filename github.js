59128dd2ddf90c75e6080cbd236fc19d4514304f
Bogdan Parubok <bogdan.parubok@hotmail.com>
2024-04-28 16:12:50 +0300

    Bumps bootstrap version, removes unnecessary imports, minor and UI code corrections.

--- github.js
+++ github.js
@@ -12,8 +12,6 @@ class GitHub
 
     async GetUserData()
     {
-        //backtick - ``
-        //shift + button before backspace
         let urlProfile = `${this.apiUrl}${this.user}?client_id=${this.client_ID}&client_secret=${this.client_secret}`;
         let urlRepos = `${this.apiUrl}${this.user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_ID}&client_secret=${this.client_secret}`;
 
--- ui.js
+++ ui.js
@@ -18,21 +18,21 @@ class UI
                 </div>
 
                 <div class="col-md-9">
-                    <span class="badge badge-primary"><b>Public Repos:</b>${user.public_repos}</span>
-                    <span class="badge badge-success"><b>Followers:</b>${user.followers}</span>
-                    <span class="badge badge-warning"><b>Following:</b>${user.following}</span>
+                    <span class="badge badge-primary"><b>Public Repos: </b>${user.public_repos}</span>
+                    <span class="badge badge-success"><b>Followers: </b>${user.followers}</span>
+                    <span class="badge badge-warning"><b>Following: </b>${user.following}</span>
 
                     <ul class="list-group">
-                        <li class="list-group-item"><b>Website/Blog:</b>${user.blog}</li>
-                        <li class="list-group-item"><b>Location:</b>${user.location}</li>
-                        <li class="list-group-item"><b>Member since:</b>${user.created_at}</li>
-                        <li class="list-group-item"><b>Bio:</b>${user.bio}</li>
+                        <li class="list-group-item"><b>Website/Blog: </b>${user.blog}</li>
+                        <li class="list-group-item"><b>Location: </b>${user.location}</li>
+                        <li class="list-group-item"><b>Member since: </b>${user.created_at}</li>
+                        <li class="list-group-item"><b>Bio: </b>${user.bio}</li>
         `;
 
         if(user.hireable)
-            bioHTML += `<li class="list-group-item"><b>Hireable:</b>Yes</li>`;
+            bioHTML += `<li class="list-group-item"><b>Hireable: </b>Yes</li>`;
         else
-            bioHTML += `<li class="list-group-item"><b>Hireable:</b>No</li>`;
+            bioHTML += `<li class="list-group-item"><b>Hireable: </b>No</li>`;
         bioHTML += `
                     </ul>
                 </div>                          
