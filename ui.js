99ee01331daed17d616745efacb44b1508ab8216
Bogdan Parubok <bogdan.parubok@hotmail.com>
2024-04-28 11:57:31 +0300

    Updates design of UI.

--- ui.js
+++ ui.js
@@ -14,32 +14,32 @@ class UI
                 <div class="col-md-3">
                     <h3 class="font-weight-bold text-info">${user.login}</h3>
                     <img class="img-fluid mb-2" src="${user.avatar_url}">
-                    <a class="btn btn-primary btn-block" target="_blank" href="${user.html_url}"> Go to Profile </a>
+                    <a class="btn btn-primary btn-block" target="_blank" href="${user.html_url}">Go to Profile</a>
                 </div>
 
                 <div class="col-md-9">
-                    <span class="badge badge-primary"> Public Repos: ${user.public_repos}</span>
-                    <span class="badge badge-success"> Followers: ${user.followers}</span>
-                    <span class="badge badge-warning"> Following: ${user.following}</span>
+                    <span class="badge badge-primary"><b>Public Repos:</b>${user.public_repos}</span>
+                    <span class="badge badge-success"><b>Followers:</b>${user.followers}</span>
+                    <span class="badge badge-warning"><b>Following:</b>${user.following}</span>
 
                     <ul class="list-group">
-                        <li class="list-group-item"> Website/Blog: ${user.blog}</li>
-                        <li class="list-group-item"> Location: ${user.location}</li>
-                        <li class="list-group-item"> Member since: ${user.created_at}</li>
-                        <li class="list-group-item"> Bio: ${user.bio}</li>
+                        <li class="list-group-item"><b>Website/Blog:</b>${user.blog}</li>
+                        <li class="list-group-item"><b>Location:</b>${user.location}</li>
+                        <li class="list-group-item"><b>Member since:</b>${user.created_at}</li>
+                        <li class="list-group-item"><b>Bio:</b>${user.bio}</li>
         `;
 
         if(user.hireable)
-            bioHTML += `<li class="list-group-item"> Hireable: Yes</li>`;
+            bioHTML += `<li class="list-group-item"><b>Hireable:</b>Yes</li>`;
         else
-            bioHTML += `<li class="list-group-item"> Hireable: No</li>`;
+            bioHTML += `<li class="list-group-item"><b>Hireable:</b>No</li>`;
         bioHTML += `
                     </ul>
                 </div>                          
             </div>
         </div>
 
-        <h3 class="page-heading mb-3"> Latest repos</h3>
+        <h3 class="page-heading mb-3">Latest repos</h3>
         <div id="repos"></div>
         `;
         this.profile.innerHTML = bioHTML;
@@ -51,13 +51,17 @@ class UI
 
         repos.forEach(repo => {
             reposArrayElement += `
-                <div class="card card-body mb-2">
+                <div class="card card-body mb-3">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-3">
                             <a href="${repo.html_url}" target="_blank"> ${repo.name}</a>
                         </div>
 
-                        <div class="col-md-6">
+                        <div class="col-md-7">
+                            <span>${repo.description}</span>
+                        </div>
+
+                        <div class="col-md-2">
                             <span class="badge badge-primary"> Followers: ${repo.forks_count}</span>
                         </div>
                     </div>
